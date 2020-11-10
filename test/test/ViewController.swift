d33bffaac5e186ed04d9d875a53cc15aee055a04
JustinCCampbell3 <campbjus@oregonstate.edu>
2020-11-10 13:28:25 -0800

    code comments

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    //Variable to hold username
     @IBOutlet weak var userNameField: UITextField!
     
-    
+    //Variable to hold password
     @IBOutlet weak var passwordField: UITextField!
     
+    //TextBox input variable
     @IBOutlet weak var textView: UITextView!;
     
     
@@ -26,10 +27,11 @@ class ViewController: UIViewController {
         userNameField.delegate = self
         passwordField.delegate = self
         
+        //Hides password entry
         passwordField.isSecureTextEntry = true
     }
     
-   
+   //function prints username and password to screen once "View Results" is hit
     @IBAction func enterTapped(_ sender: Any) {
         textView.text = "UserName: \(userNameField.text!)\nPassword: \(passwordField.text!)"
     }
