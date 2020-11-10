00feadffda06ebae71c7c0829f1100d34dc3f4b8
JustinCCampbell3 <campbjus@oregonstate.edu>
2020-11-10 12:51:23 -0800

    First Login Page update, still working on password hide

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -9,9 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userNameField: UITextField!
+    
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!;
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        userNameField.delegate = self
+        passwordField.delegate = self
+    }
+    
+   
+    @IBAction func enterTapped(_ sender: Any) {
+        textView.text = "UserName: \(userNameField.text!)\nPassword: \(passwordField.text!)"
+    }
+   
+}
+
+extension ViewController : UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
 
