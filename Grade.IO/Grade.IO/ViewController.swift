ba407763e439fb9b5e1ba18db8198ad99aab0448
matt <mattgibbs19@gmail.com>
2020-11-22 06:29:42 -0800

    New XCode Project with firebase. got it about 1000x smaller lol

---
+++ Grade.IO/Grade.IO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grade.IO
+//
+//  Created by user183542 on 11/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(Bundle.main.bundleIdentifier)
+    }
+
+
+}
+
