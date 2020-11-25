5fe6059dff2c16a420dc026f5328f8abd615a06c
JustinCCampbell3 <campbjus@oregonstate.edu>
2020-11-25 15:01:44 -0800

    Added login, create an account, and login successful pages. Partial functionality. Need firebase conditional statements to finish linking pages.

--- Grade.IO/Grade.IO/ViewController.swift
+++ Grade.IO/Grade.IO/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //setting variables for email and password on login screen
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+   
+    
     override func viewDidLoad() {
+        emailField.delegate = self
+        passwordField.delegate = self
+        
         super.viewDidLoad()
         print(Bundle.main.bundleIdentifier)
+        
     }
 
-
+    //this function handles input on the login screen
+    @IBAction func loginPressed(_ sender: Any) {
+     //there are two variables that can be accessed here
+     //Email: "emailField.text!"
+     //Password: "passwordField.text
+    }
+    
+    //this function handles input on the create an account page
+    @IBAction func createAccountPressed(_ sender: Any) {
+     //variables are being funky on this page so they will be soon to come
+        
+    }
+    
+    //this function handles when the google sign in is pressed
+    @IBAction func googleSignInPressed(_ sender: Any) {
+    
+        
+    }
+    
 }
 
+extension ViewController : UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+}
