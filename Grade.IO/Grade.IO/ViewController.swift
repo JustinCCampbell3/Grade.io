8fc6df93cefe38c586e38af1ddac86b83cd95533
matt <mattgibbs19@gmail.com>
2020-11-29 01:25:04 -0800

    Making basic auth functions. Saving some code for later

--- Grade.IO/Grade.IO/ViewController.swift
+++ Grade.IO/Grade.IO/ViewController.swift
@@ -17,13 +17,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var signInButton: GIDSignInButton!
 
-    
     override func viewDidLoad() {
         emailField.delegate = self
         passwordField.delegate = self
-        GIDSignIn.sharedInstance()?.presentingViewController = self
-        GIDSignIn.sharedInstance().signIn()
-        
+        googleOnStartLoginWork()
         super.viewDidLoad()
     }
 
@@ -32,29 +29,33 @@ class ViewController: UIViewController {
      //there are two variables that can be accessed here
      //Email: "emailField.text!"
      //Password: "passwordField.text
-        
-        //Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { authResult, error in
-        //}   To create user
-        //Auth.auth().signIn(withEmail:  emailField.text!, password: passwordField.text!) { [weak self] authResult, error in
-        // guard let strongSelf = self else { return }
-        // print("Fuck")
-        //} To sign in
     }
     
     //this function handles input on the create an account page
     @IBAction func createAccountPressed(_ sender: Any) {
      //variables are being funky on this page so they will be soon to come
-        
     }
     
     //this function handles when the google sign in is pressed
-    @IBAction func googleSignInPressed(_ sender: Any) {
-        
-        
+    // MATT 11/29/2020 : I don't think we need this anymore.
+    @IBAction func googleSignInPressed(_ sender: Any) {}
+    
+    /// calls to make on ViewDidLoad for google auth
+    func googleOnStartLoginWork() {
+        GIDSignIn.sharedInstance()?.presentingViewController = self
     }
     
-    
-    
+    /// Call to sign out.. Could there be an AuthHelper class to put this in?
+    func signOutWithErrorCatch() {
+        do {
+            try Auth.auth().signOut()
+        }
+        catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+        print ("Signed out successfully")
+    }
 }
 
 extension ViewController : UITextFieldDelegate{
@@ -63,6 +64,5 @@ extension ViewController : UITextFieldDelegate{
         textField.resignFirstResponder()
         return true
     }
-    
-    
 }
+
