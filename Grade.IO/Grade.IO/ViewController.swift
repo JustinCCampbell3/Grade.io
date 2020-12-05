65bd8ff0f62cf258c54e3d247e33823df2d7c6ff
matt <mattgibbs19@gmail.com>
2020-12-04 19:31:15 -0800

    Create account page actually creates an account, log in works correctly
    also, signout button works and takes user back to sign in page

--- Grade.IO/Grade.IO/ViewController.swift
+++ Grade.IO/Grade.IO/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController{
     @IBOutlet weak var passwordField: UITextField!
     
     @IBOutlet weak var signInButton: GIDSignInButton!
-
+    
     //variable for calendar function
     //@IBOutlet var calendarView:FSCalendar!
     
@@ -26,9 +26,9 @@ class ViewController: UIViewController{
         passwordField.delegate = self
         googleOnStartLoginWork()
         super.viewDidLoad()
-        
         //setupCalendar()
-        
+        checkCredentials()
+
     }
 
     //func setupCalendar(){
@@ -38,9 +38,8 @@ class ViewController: UIViewController{
     
     //this function handles input on the login screen
     @IBAction func loginPressed(_ sender: Any) {
-     //there are two variables that can be accessed here
-     //Email: "emailField.text!"
-     //Password: "passwordField.text
+        signInWithEmail(email: emailField.text!, password: passwordField.text!)
+        checkCredentials()
     }
     
     //this function handles input on the create an account page
@@ -55,8 +54,36 @@ class ViewController: UIViewController{
     /// calls to make on ViewDidLoad for google auth
     func googleOnStartLoginWork() {
         GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance().signIn()
+        checkCredentials()
     }
     
+    func checkCredentials() {
+        if (Auth.auth().currentUser != nil) {
+            showHomeScreen_Student()
+        }
+    }
+    
+    func showHomeScreen_Student() {
+        performSegue(withIdentifier: "signInToHomeScreen_STUDENT", sender: self)
+    }
+    
+    // TO SIGN IN  WITH EMAIL
+   func signInWithEmail(email: String, password: String) {
+        Auth.auth().signIn(withEmail:  email, password: password) { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+        }
+    }
+    
+    func signOutWithErrorCatch() {
+        do {
+            try Auth.auth().signOut()
+        }
+        catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+
 }
 
 extension ViewController : UITextFieldDelegate{
