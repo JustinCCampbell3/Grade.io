fa406498e752e91cec74dfea63ca7275011b430b
Remote Learning Team <cabralep@oregonstate.edu>
2021-01-05 23:00:02 -0800

    fixed issues caused by tab controller and nav bar controller. Also started updating the zoom page (most functionality will come from the teacher view)

---
+++ Grade.IO/Grade.IO/NavigationView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationView.swift
+//  Grade.IO
+//
+//  Created by user183573 on 1/5/21.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView()
+    }
+}
---
+++ Grade.IO/Grade.IO/ZoomViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ZoomViewController.swift
+//  Grade.IO
+//
+//  Created by user183573 on 1/5/21.
+//
+
+import UIKit
+
+class ZoomViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Zoom Button"
+    }
+}
