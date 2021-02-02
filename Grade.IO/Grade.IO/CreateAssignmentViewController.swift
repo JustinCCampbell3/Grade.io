f05265f3593cb569f9ae364a7cd94f5d05b8befe
Remote Learning Team <cabralep@oregonstate.edu>
2021-02-02 12:13:56 -0800

    document uploader and assignment creation page updates

--- Grade.IO/Grade.IO/CreateAssignmentViewController.swift
+++ Grade.IO/Grade.IO/CreateAssignmentViewController.swift
@@ -7,13 +7,19 @@
 import UIKit
 import MobileCoreServices
 import UniformTypeIdentifiers
+import Firebase
 
-class CreateAssignmentViewController: UIViewController, UIDocumentPickerDelegate {
-
+class CreateAssignmentViewController: UIViewController, UIDocumentPickerDelegate{
+    //variables for the text fields found on this page
+    @IBOutlet weak var assignName: UITextField!
+    @IBOutlet weak var assignDueDate: UITextField!
+    @IBOutlet weak var assignInstructions: UITextView!
+    
+    //variable for the label that will hold the name of the file uploaded
+    @IBOutlet weak var fileNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     //allows func to be added to a button so that when its clicked the file picker will be brought up
@@ -24,8 +30,84 @@ class CreateAssignmentViewController: UIViewController, UIDocumentPickerDelegate
         let documentPickerController = UIDocumentPickerViewController(forOpeningContentTypes: types)
         //delegate it to itself
         documentPickerController.delegate = self
+        //only let them pick one thing
+        documentPickerController.allowsMultipleSelection = false;
         //show the document pick controller (file app)
         self.present(documentPickerController, animated: true, completion: nil)
+    }
+    
+    //when the document has been picked, this is what we want it to do with it
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        print("inside didPickDocumentsAt")
+        print(urls)
+        
+        
+        
+        //creation of an alert to show the user that their document was uploaded
+        let fileAlert = UIAlertController(title: "Completed", message: "File has been successfully uploaded", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .default, handler: {(action) -> Void in print("ok button pressed")})
+        fileAlert.addAction(ok)
+        
+        //create a variable for the storage we are referencing in Firebase
+        let bucket = Storage.storage().reference()
+        
+        //throw the url/file to the firebase storage we have
+        bucket.child((urls.first?.deletingPathExtension().lastPathComponent)!).putFile(from: urls.first!, metadata: nil){ (_, err) in
+            
+            //if there's an error, print the reason
+            if err != nil{
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            print("success")
+            
+            //get the name of the file imported
+            let fileNameURL: String? = urls.first?.lastPathComponent
+            //change the label text to the name of the file we got
+            self.fileNameLabel.text = fileNameURL!
+
+            
+            //if the file successfully was pushed to the storage, then present the popup
+            self.present(fileAlert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    //if the user cancels out of the document picker, then dismiss the controller
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("Document picker cancelled")
+        //dismiss the document picker controller (file app)
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    //when the confirm button is clicked, grab the text from the text fields, throw it to the database
+    //figure out how to get this and the uploaded file to then be grabbed together later
+    @IBAction func confirmQuizCreation(){
+        print("label name before: " + fileNameLabel.text!)
+        //check if any field is empty, and if so, don't let them do any of the below
+        if(assignName.text == "" || assignDueDate.text == "" || assignInstructions.text == "" || fileNameLabel.text == ""){
+            print("inside")
+        }
+        else{
+            //grabbing the text of the assignment name text field
+            let assignNameText: String? = assignName.text
+            print("Assignment Name: " + assignNameText!)
+            
+            //grabbing the text of the assignment name text field
+            let assignDueDateText: String? = assignDueDate.text
+            print("Assignment Due Date: " + assignDueDateText!)
+            
+            //grabbing the text of the assignment name text field
+            let assignInstructionText: String? = assignInstructions.text
+            print("Assignment Instructions: " + assignInstructionText!)
+            
+            //grabbing the text of the assignment file uploaded so it can be found later
+            let fileNameLabelText: String? = fileNameLabel.text
+            print("File Name from Label: " + fileNameLabelText!)
+        }
+        
+        
     }
 
 }
