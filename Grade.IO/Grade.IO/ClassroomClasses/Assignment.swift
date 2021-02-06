cadc583aab55eccfa6dc84987bf1e80d2ac6f6d0
matt <mattgibbs19@gmail.com>
2021-02-05 19:05:52 -0800

    Actual files for assignment and result

--- Grade.IO/Grade.IO/ClassroomClasses/Assignment.swift
+++ Grade.IO/Grade.IO/ClassroomClasses/Assignment.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+public struct Problem {
+    public var Question:String = ""
+    public var Answer:String = ""
+}
+
+public class Assignment {
+    public var Results:[Result]
+    public var Problems:[Problem]
+    public var DueDate:Date
+    public var ID:String
+    public var ClassID:String
+    public var FilePath:String
+    public var Overview:String
+    
+    public init() {
+        Results = []
+        Problems = []
+        DueDate = Date.init()
+        ID = ""
+        ClassID = ""
+        FilePath = ""
+        Overview = ""
+    }
+    
+    public func AddResult(newResult:Result) {
+        Results.append(newResult)
+    }
+    public func AddProblem(newProblem:Problem) {
+        Problems.append(newProblem)
+    }
+    public func GetAverageTime() -> TimeInterval {
+        var sum = 0.0
+        for result in Results {
+            sum += result.CompletionTime
+        }
+        return (sum/(Double(Results.count)))
+    }
+}
--- Grade.IO/Grade.IO/ClassroomClasses/Result.swift
+++ Grade.IO/Grade.IO/ClassroomClasses/Result.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+public class Result {
+    var CompletionTime:TimeInterval
+    var StudentID:String
+    var AssignmentID:String
+    var Grade:Float
+    var IsSubmitted:Bool
+    
+    public init() {
+        CompletionTime = 0.0
+        Grade = 0.0
+        IsSubmitted = false
+        StudentID = ""
+        AssignmentID = ""
+    }
+    
+    public func SetCompletionTime(newTime:TimeInterval) {
+        
+    }
+    public func SetGrade(newGrade:Float) {
+        
+    }
+    public func SetStudentID(newStudentID:String) {
+        
+    }
+    public func SetAssignmentID(newAssignmentID:String) {
+        
+    }
+}
