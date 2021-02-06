00d6f01da2a8ea254ff33636b7f6fc01c4219a48
Remote Learning Team <cabralep@oregonstate.edu>
2021-02-06 12:19:04 -0800

    Student View has basic UI of all currently created pages

---
+++ Grade.IO/Grade.IO/StudentAssignmentsView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentAssignmentsView.swift
+//  Grade.IO
+//
+//  Created by user183573 on 2/3/21.
+//
+
+import UIKit
+
+class StudentAssignmentsView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
