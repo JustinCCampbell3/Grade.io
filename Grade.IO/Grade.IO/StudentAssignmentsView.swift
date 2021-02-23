73e3c0492da5785f8afa112388fda219a73efb3b
Remote Learning Team <cabralep@oregonstate.edu>
2021-02-22 23:05:19 -0800

    Working scroll view for teacher assignments besides date. Students need toggle between todo and complete assignments

--- Grade.IO/Grade.IO/StudentAssignmentsView.swift
+++ Grade.IO/Grade.IO/StudentAssignmentsView.swift
@@ -9,8 +9,256 @@ import UIKit
 
 class StudentAssignmentsView: UIViewController {
 
+    //array of views
+    var todoViewArray:[UIView] = []
+    //array of completed views
+    var completeViewArray:[UIView] = []
+    
+    //UILabel for ToDo
+    @IBOutlet weak var todoLabel: UILabel!
+    //UILabel for Completed
+    @IBOutlet weak var completeLabel: UILabel!
+
+    //scroll view to hold all assignments that still need to be done
+    @IBOutlet lazy var todoScrollView: UIScrollView! = {
+        let view = UIScrollView(frame: CGRect(x: 0, y: (todoLabel.frame.origin.y + todoLabel.frame.height + 8), width: self.view.frame.width, height: 315))
+        //view.contentSize = contentViewSize
+        view.autoresizingMask = .flexibleHeight
+        view.bounces = true
+        view.showsHorizontalScrollIndicator = true
+        return view
+    }()
+    
+    //scroll view to hold all assignments that still need to be done
+    @IBOutlet lazy var completeScrollView: UIScrollView! = {
+        let view = UIScrollView(frame: CGRect(x: 0, y: (completeLabel.frame.origin.y + completeLabel.frame.height + 8), width: self.view.frame.width, height: self.view.frame.height))
+        //view.contentSize = contentViewSize
+        view.autoresizingMask = .flexibleHeight
+        view.bounces = true
+        view.showsHorizontalScrollIndicator = true
+        return view
+    }()
+    
+    //var that will hold the assignments
+    var todoAssignments: [Assignment] = []
+    //second var to hold assignments
+    var completeAssignments: [Assignment] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //create a scroll view based on the number of assignments we have
+        //may have to change testClass to myClass later
+        DatabaseHelper.GetAssignmentsFromClassID(classID: "testClass") { (res) in
+            self.makeTodoScrollView(newList: res)
+            self.makeCompleteScrollView(newList: res)
+        }
+    }
+    
+    //change listAssignments to have all the assignments
+    private func getTodoAssignmentList(assignments: [Assignment]){
+        for i in assignments {
+            todoAssignments.append(i)
+        }
+    }
+    
+    //change completeAssignments to have all assignments
+    private func getCompleteAssignmentList(assignments: [Assignment]){
+        for i in assignments {
+            completeAssignments.append(i)
+        }
+    }
+    
+    private func makeTodoScrollView(newList: [Assignment]){
+        //make a list of assignments
+        self.getTodoAssignmentList(assignments: newList)
+        
+        //size of the content needed
+        var contentViewSize = CGSize(width: self.view.frame.width, height: self.view.frame.size.height)
+        
+        //var that will hold the new size of the height
+        var newHeight = CGFloat(0)
+        
+        //add a UIView for all the assignments we have
+        for i in 0..<todoAssignments.count{
+            print("in for loop where i is: ", i)
+            var multiplier = CGFloat(i)
+            if(i == 1){
+                multiplier = 150
+            }
+            else if(i > 1){
+                multiplier = (CGFloat(i)*100) + (50 * CGFloat(i))
+            }
+            
+            
+            print("y is: ", multiplier)
+            let containerView: UIView = {
+                let view = UIView(frame: CGRect(x: 0, y: multiplier, width: self.view.frame.width, height: 100))
+                //this is the color of the scroll view background, not the actual full container
+                view.backgroundColor = .white
+                //view.frame.size = contentViewSize
+                return view
+            }()
+            todoViewArray.append(containerView)
+            print("i is: ", i)
+            newHeight = multiplier
+        }
+        print("newHeight: ", newHeight)
+        
+        if(newHeight > CGFloat(self.view.frame.size.height)){
+            contentViewSize = CGSize(width: self.view.frame.width, height: newHeight + 350) //100 height + 150 in between of stuff
+            print("newHeight is larger than the current frame size")
+        }
+        todoScrollView.contentSize = contentViewSize
+        
+        //all scroll view stuff down here
+        todoScrollView.layoutIfNeeded()
+        todoScrollView.isScrollEnabled = true
+        
+        view.addSubview(todoScrollView)
+        
+        //keep track of the current index
+        var curIndex = 0
+        
+        //put the info necessary within the assignment views
+        for i in todoViewArray {
+            todoScrollView.addSubview(i)
+
+            //making label here makes sure each view has their own label
+            //label for assignment name
+            let nameLabel: UILabel = {
+                let label = UILabel()
+                label.text = todoAssignments[curIndex].name
+                return label
+            }()
+            i.addSubview(nameLabel)
+            nameLabel.left(to: i, offset: 30)
+            nameLabel.top(to: i, offset: (i.frame.height/4)-nameLabel.frame.height)
+            
+            //section for changing date to string
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yy"
+            //print("before date: ", todoAssignments[curIndex].dueDate!)
+            //print("Date: ", dateFormatter.string(from: listAssignments[curIndex].dueDate!))
+            
+            //label for due date
+            let dueDateLabel: UILabel = {
+                let label = UILabel()
+                label.text = "Date"
+                return label
+            }()
+            i.addSubview(dueDateLabel)
+            dueDateLabel.left(to: i, offset: 30)
+            dueDateLabel.top(to: i, offset: (i.frame.height/2)-nameLabel.frame.height) //halfway down the view
+            
+            
+            
+            //lines that allow the view to be tapped
+            let gesture = UITapGestureRecognizer(target: self, action: #selector(self.sendToAssignment(_sender:)))
+            i.addGestureRecognizer(gesture)
+            
+            curIndex+=1
+        }
+    }
+    
+    private func makeCompleteScrollView(newList: [Assignment]){
+        //make a list of assignments
+        self.getCompleteAssignmentList(assignments: newList)
+        
+        //size of the content needed
+        var contentViewSize = CGSize(width: self.view.frame.width, height: self.view.frame.size.height)
+        
+        //var that will hold the new size of the height
+        var newHeight = CGFloat(0)
+        
+        //add a UIView for all the assignments we have
+        for i in 0..<completeAssignments.count{
+            print("in for loop where i is: ", i)
+            var multiplier = CGFloat(i)
+            if(i == 1){
+                multiplier = 150
+            }
+            else if(i > 1){
+                multiplier = (CGFloat(i)*100) + (50 * CGFloat(i))
+            }
+            
+            
+            print("y is: ", multiplier)
+            let containerView: UIView = {
+                let view = UIView(frame: CGRect(x: 0, y: multiplier, width: self.view.frame.width, height: 100))
+                //this is the color of the scroll view background, not the actual full container
+                view.backgroundColor = .white
+                //view.frame.size = contentViewSize
+                return view
+            }()
+            completeViewArray.append(containerView)
+            print("i is: ", i)
+            newHeight = multiplier
+        }
+        print("newHeight: ", newHeight)
+        
+        if(newHeight > CGFloat(self.view.frame.size.height)){
+            contentViewSize = CGSize(width: self.view.frame.width, height: newHeight + 350) //100 height + 150 in between of stuff
+            print("newHeight is larger than the current frame size")
+        }
+        completeScrollView.contentSize = contentViewSize
+        
+        //all scroll view stuff down here
+        completeScrollView.layoutIfNeeded()
+        completeScrollView.isScrollEnabled = true
+        
+        view.addSubview(completeScrollView)
+        
+        //keep track of the current index
+        var index = 0
+        
+        //put the info necessary within the assignment views
+        for i in completeViewArray {
+            completeScrollView.addSubview(i)
+
+            //making label here makes sure each view has their own label
+            //label for assignment name
+            let nameLabel: UILabel = {
+                let label = UILabel()
+                label.text = completeAssignments[index].name
+                return label
+            }()
+            i.addSubview(nameLabel)
+            nameLabel.left(to: i, offset: 30)
+            nameLabel.top(to: i, offset: (i.frame.height/4)-nameLabel.frame.height)
+            
+            //section for changing date to string
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yy"
+            //print("before date: ", completeAssignments[curIndex].dueDate!)
+            //print("Date: ", dateFormatter.string(from: listAssignments[curIndex].dueDate!))
+            
+            //label for due date
+            let dueDateLabel: UILabel = {
+                let label = UILabel()
+                label.text = "Date"
+                return label
+            }()
+            i.addSubview(dueDateLabel)
+            dueDateLabel.left(to: i, offset: 30)
+            dueDateLabel.top(to: i, offset: (i.frame.height/2)-nameLabel.frame.height) //halfway down the view
+            
+            
+            
+            //lines that allow the view to be tapped
+            let gesture = UITapGestureRecognizer(target: self, action: #selector(self.sendToAssignment(_sender:)))
+            i.addGestureRecognizer(gesture)
+            
+           index+=1
+        }
+    }
+    
+    //send the user to the assignment page when they click a UIView
+    @objc func sendToAssignment(_sender:UITapGestureRecognizer){
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc: UIViewController = storyBoard.instantiateViewController(withIdentifier: "TAssignSpec") as! UIViewController
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated:true, completion: nil)
     }
     
 }
--- Grade.IO/Grade.IO/TeacherAssignmentPage.swift
+++ Grade.IO/Grade.IO/TeacherAssignmentPage.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SideMenu
 import TinyConstraints
+import Foundation
 
 class TeacherAssignmentPage: UIViewController, MenuControllerDelegate {
     //variable for the slide out menu in the teacher view
@@ -20,8 +21,7 @@ class TeacherAssignmentPage: UIViewController, MenuControllerDelegate {
     //add assignment button that will be used for the upper boundary
     @IBOutlet var addAssignBtn: UIButton!
     
-    //the width and size var for the
-    //lazy var contentViewSize = CGSize(width: self.view.frame.width, height: self.view.frame.size.height + 2000) //need number of assignments to then get how big the scroll view should be (Ask Matt!!!!!!!!!)
+
     //scroll view to hold everything
     lazy var scrollView: UIScrollView! = {
         let view = UIScrollView(frame: CGRect(x: 0, y: (addAssignBtn.frame.origin.y * 2)  + (addAssignBtn.frame.height), width: self.view.frame.width, height: self.view.frame.height))
@@ -32,74 +32,17 @@ class TeacherAssignmentPage: UIViewController, MenuControllerDelegate {
         return view
     }()
     
-    //stack view to hold UIViews within
-    lazy var stackView: UIStackView = {
-        let view = UIStackView()
-        view.alignment = .center
-        view.axis = .vertical
-        view.distribution = .equalSpacing
-        view.spacing = 120.0
-        return view
-    }()
-    
-        /*lazy var containerView: UIView = {
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100))
-        //this is the color of the scroll view background, not the actual full container
-        //I hate life
-        view.backgroundColor = .systemGreen
-        //view.frame.size = contentViewSize
-        return view
-    }()*/
-    
-    lazy var label: UILabel = {
-        let label = UILabel()
-        label.text = "Assignment"
-        return label
-    }()
+    //var that will hold the assignments
+    var listAssignments: [Assignment] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("current frame size height:", self.view.frame.size.height)
-        
-        var contentViewSize = CGSize(width: self.view.frame.width, height: self.view.frame.size.height)
-        //let contentViewSize = CGSize(width: self.view.frame.width, height: self.view.frame.size.height + (120*10)) //height of UIView * # of assignments
-        //scrollView.contentSize = contentViewSize
-        
-        var newHeight = CGFloat(0)
-        
-        //add a UIView for all the assignments we have
-        for i in 0..<15{
-            var multiplier = CGFloat(i)
-            if(i == 1){
-                multiplier = 150
-            }
-            else if(i > 1){
-                multiplier = (CGFloat(i)*100) + (50 * CGFloat(i))
-            }
-            
-            
-            print("y is: ", multiplier)
-            let containerView: UIView = {
-                let view = UIView(frame: CGRect(x: 0, y: multiplier, width: self.view.frame.width, height: 100))
-                //this is the color of the scroll view background, not the actual full container
-                //I hate life
-                view.backgroundColor = .white
-                //view.frame.size = contentViewSize
-                return view
-            }()
-            viewArray.append(containerView)
-            print("i is: ", i)
-            newHeight = multiplier
+        //create a scroll view based on the number of assignments we have
+        //may have to change testClass to myClass later
+        DatabaseHelper.GetAssignmentsFromClassID(classID: "testClass") { (res) in
+            self.makeScrollView(newList: res)
         }
-        print("newHeight: ", newHeight)
-        
-        if(newHeight > CGFloat(self.view.frame.size.height)){
-            contentViewSize = CGSize(width: self.view.frame.width, height: newHeight + 350) //100 height + 150 in between of stuff
-            print("newHeight is larger than the current frame size")
-        }
-        scrollView.contentSize = contentViewSize
-        
         
         //creating the side menu starts here
         let menu = MenuListController()
@@ -116,34 +59,105 @@ class TeacherAssignmentPage: UIViewController, MenuControllerDelegate {
         SideMenuManager.default.leftMenuNavigationController = sideMenu
         
         //SideMenuManager.default.addPanGestureToPresent(toView: self.view)
+    }
+    
+    //change listAssignments to have all the assignments
+    private func getAssignmentList(assignments: [Assignment]){
+        for i in assignments {
+            listAssignments.append(i)
+        }
+    }
+    
+    private func makeScrollView(newList: [Assignment]){
+        //make a list of assignments
+        self.getAssignmentList(assignments: newList)
         
+        //size of the content needed
+        var contentViewSize = CGSize(width: self.view.frame.width, height: self.view.frame.size.height)
+        
+        //var that will hold the new size of the height
+        var newHeight = CGFloat(0)
+        
+        //add a UIView for all the assignments we have
+        for i in 0..<listAssignments.count{
+            print("in for loop where i is: ", i)
+            var multiplier = CGFloat(i)
+            if(i == 1){
+                multiplier = 150
+            }
+            else if(i > 1){
+                multiplier = (CGFloat(i)*100) + (50 * CGFloat(i))
+            }
+            
+            
+            print("y is: ", multiplier)
+            let containerView: UIView = {
+                let view = UIView(frame: CGRect(x: 0, y: multiplier, width: self.view.frame.width, height: 100))
+                //this is the color of the scroll view background, not the actual full container
+                view.backgroundColor = .white
+                //view.frame.size = contentViewSize
+                return view
+            }()
+            viewArray.append(containerView)
+            print("i is: ", i)
+            newHeight = multiplier
+        }
+        print("newHeight: ", newHeight)
+        
+        if(newHeight > CGFloat(self.view.frame.size.height)){
+            contentViewSize = CGSize(width: self.view.frame.width, height: newHeight + 350) //100 height + 150 in between of stuff
+            print("newHeight is larger than the current frame size")
+        }
+        scrollView.contentSize = contentViewSize
         
         //all scroll view stuff down here
         scrollView.layoutIfNeeded()
         scrollView.isScrollEnabled = true
         
         view.addSubview(scrollView)
-        //scrollView.addSubview(stackView)
-        //scrollView.addSubview(containerView)
         
-        //containerView.addSubview(label)
-        //label.center(in: containerView)
+        //keep track of the current index
+        var curIndex = 0
         
+        //put the info necessary within the assignment views
         for i in viewArray {
             scrollView.addSubview(i)
-            //label only adding to the last UIView
-            //little bitch
-            i.addSubview(label)
-            label.center(in: i)
+
+            //making label here makes sure each view has their own label
+            //label for assignment name
+            let nameLabel: UILabel = {
+                let label = UILabel()
+                label.text = listAssignments[curIndex].name
+                return label
+            }()
+            i.addSubview(nameLabel)
+            nameLabel.left(to: i, offset: 30)
+            nameLabel.top(to: i, offset: (i.frame.height/4)-nameLabel.frame.height)
+            
+            //section for changing date to string
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yy"
+            //print("before date: ", listAssignments[curIndex].dueDate!)
+            //print("Date: ", dateFormatter.string(from: listAssignments[curIndex].dueDate!))
+            
+            //label for due date
+            let dueDateLabel: UILabel = {
+                let label = UILabel()
+                label.text = "Date"
+                return label
+            }()
+            i.addSubview(dueDateLabel)
+            dueDateLabel.left(to: i, offset: 30)
+            dueDateLabel.top(to: i, offset: (i.frame.height/2)-nameLabel.frame.height) //halfway down the view
+            
+            
             
             //lines that allow the view to be tapped
             let gesture = UITapGestureRecognizer(target: self, action: #selector(self.sendToAssignment(_sender:)))
-            i.addGestureRecognizer(gesture)        }
-        
-        
-        //lines that allow the view to be tapped
-        //let gesture = UITapGestureRecognizer(target: self, action: #selector(self.sendToAssignment(_sender:)))
-        //self.containerView.addGestureRecognizer(gesture)
+            i.addGestureRecognizer(gesture)
+            
+            curIndex+=1
+        }
     }
     
     //send the user to the assignment page when they click a UIView
@@ -151,7 +165,8 @@ class TeacherAssignmentPage: UIViewController, MenuControllerDelegate {
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let vc: UIViewController = storyBoard.instantiateViewController(withIdentifier: "TAssignSpec") as! UIViewController
         vc.modalPresentationStyle = .fullScreen
-        self.present(vc, animated:true, completion: nil)    }
+        self.present(vc, animated:true, completion: nil)
+    }
     
     @IBOutlet weak var name: UILabel!
     
